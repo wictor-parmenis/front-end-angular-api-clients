e37ac4e454b4d64050be8f6c327df1ffe679e762
wictor-parmenis <wictortec@gmail.com>
2021-09-16 15:55:43 -0300

    add simple validators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,24 +11,54 @@ import { NgForm, FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class AppComponent {
   title = 'front-end-api-calcme'
-
+  cadasterForm = new FormGroup({
+    email: new FormControl('', Validators.required),
+    name: new FormControl('', Validators.required),
+    tel: new FormControl('', Validators.required)
+  })
 
   constructor (private registerService: CadasterService) {
   }
 
-    cadaster(form: NgForm) {
-      console.log(form.value);
+  get email() {
+    return this.cadasterForm.get('email')
+  }
 
-        this.registerService.cadaster(form.value.name,  form.value.phone, form.value.email).subscribe((res) => {
-          alert('registrado com sucesso.')
-          console.log(res)
-          
-          
-        }, (error) => {
-          console.log(error);
-          alert('Ops, houve algum erro no seu registro.')
-          
-        })
+  get name() {
+    return this.cadasterForm.get('name')
+  }
+
+  get tel() {
+    return this.cadasterForm.get('tel')
+  }
+
+
+    onSubmit() {
+        const email = this.cadasterForm.get('email')
+        const tel = this.cadasterForm.get('tel')
+        const name = this.cadasterForm.get('name')
+        const valid = this.cadasterForm.valid 
+
+        console.log(name, tel, email);
+        console.log(this.cadasterForm.get('email')?.value);
+
+        if (valid) {
+          this.registerService.cadaster(name?.value,  tel?.value, email?.value).subscribe((res) => {
+            alert('registrado com sucesso.')            
+            this.cadasterForm.reset()
+
+          }, (error) => {
+            console.log(error);
+            alert('Ops, houve algum erro no seu registro.')
+            this.cadasterForm.reset()
+
+            
+          })
+        } 
+
+
+        
+
       
 
       
